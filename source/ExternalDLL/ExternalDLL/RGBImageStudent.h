8325839fd52ff5de7ee58915fc6fde3f69b91fbb
MaikelB <maikel.bolderdijk@student.hu.nl>
2016-04-15 18:58:22 +0200

    Added base files and added some code for week 1

---
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.h
@@ -0,0 +1,31 @@
+/*
+* Copyright (c) 2015 DottedEye Designs, Alexander Hustinx, NeoTech Software, Rolf Smit - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+*/
+
+#pragma once
+#include "RGBImage.h"
+#include <vector>
+
+class RGBImageStudent : public RGBImage {
+public:
+
+	RGBImageStudent();
+	RGBImageStudent(const RGBImageStudent &other);
+	RGBImageStudent(const int width, const int height);
+	~RGBImageStudent();
+
+	void set(const int width, const int height);
+	void set(const RGBImageStudent &other);
+
+	void setPixel(int x, int y, RGB pixel);
+	void setPixel(int i, RGB pixel);
+
+	RGB getPixel(int x, int y) const;
+	RGB getPixel(int i) const;
+
+private:
+	std::vector<int> * RGBPixelIndex = new std::vector<int>();
+	std::vector<std::pair<int[2], RGB>> * RGBLocPixel = new std::vector<std::pair<int[2], RGB>>();
+};
